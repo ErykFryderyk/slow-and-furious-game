1a5eabd688a3713f75c52a2ecee861e1f69fab69
Eryk Fryderyk <grzeniu098@gmail.com>
2021-04-22 23:14:57 +0200

    create new Class

--- src/script/OtherCar.js
+++ src/script/OtherCar.js
@@ -1,3 +1,20 @@
 export class OtherCar {
+    container(container, otherCarClass){
+        this.container = container;
+        this.otherCarClass = otherCarClass;
+        this.element = document.createElement('div');
+    }
     
+    init(){
+        this.#setCar();
+    }
+    
+    #setCar(){
+        this.element.className.add('other-car');
+    }
+
+    #randomDrivingCarPosition(){
+        return Math.floor(Math.random() * this.container.innerWidth - this.element.offsetWidth);
+    }
 }
+
