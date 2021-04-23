d2f44796758dca28253ca5c7fdb266cd3e0309ca
Eryk Fryderyk <grzeniu098@gmail.com>
2021-04-23 17:41:22 +0200

    create other car object

--- src/script/Game.js
+++ src/script/Game.js
@@ -6,11 +6,27 @@ class Game {
       car: document.querySelector('[data-car]'),
       roadway: document.querySelector('[data-roadway]')
   }  
-  #car = new Car(this.#htmlElements.car, this.#htmlElements.roadway);
+  #car = new Car(
+    this.#htmlElements.car, 
+    this.#htmlElements.roadway
+  );
+  #cars = [];
+  #createOtherCarInterval = null;
 
   init() {
     this.#car.init();
-    // this.#otherCar.init();
+    this.#newGame();
+  }
+
+  #newGame(){
+    this.#createOtherCarInterval = setInterval(() => this.#createNewOtherCar(), 5000);
+  }
+
+  #createNewOtherCar(){
+    const car = new OtherCar(this.#htmlElements.roadway, 'other-car');
+
+    car.init();
+    this.#cars.push(car);
   }
 }
 
