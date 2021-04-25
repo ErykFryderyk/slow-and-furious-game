2ced4b739f73fbea822cd0de9f274d483054b632
Eryk Fryderyk <grzeniu098@gmail.com>
2021-04-25 15:02:53 +0200

    add method responsible form create  and remove other cars

--- src/script/Game.js
+++ src/script/Game.js
@@ -11,7 +11,11 @@ class Game {
     this.#htmlElements.roadway
   );
   #cars = [];
+  #otherCarsInterval = null;
+  #checkPositionInterval = null;
   #createOtherCarInterval = null;
+  #carSpeed = 5;
+  #intervalValue = 1000;
 
   init() {
     this.#car.init();
@@ -19,14 +23,54 @@ class Game {
   }
 
   #newGame(){
-    this.#createOtherCarInterval = setInterval(() => this.#createNewOtherCar(), 5000);
+    this.#otherCarsInterval = 10;
+    // this.#createOtherCarInterval = setInterval(() => this.#randomNewOtherCar(), this.#intervalValue);
+    // this.#checkPositionInterval = setInterval(() => this.#checkPosition(), 1);
   }
 
-  #createNewOtherCar(){
-    const car = new OtherCar(this.#htmlElements.roadway, 'other-car');
+  #randomNewOtherCar(){
+    const randomNumber = Math.floor(Math.random() * 5) + 1
+    randomNumber % 5 
+    ? this.#createNewOtherCar(
+        this.#htmlElements.roadway,
+        this.#otherCarsInterval,
+        this.#carSpeed, 
+        'pickup'
+      ) 
+    : 
+      this.#createNewOtherCar(
+        this.#htmlElements.roadway,
+        this.#otherCarsInterval, 
+        this.#carSpeed,
+        'van'
+      );  
+  }
+
+  #createNewOtherCar(...params){
+    const car = new OtherCar(
+      ...params
+    );
 
     car.init();
     this.#cars.push(car);
+  }
+
+  #checkPosition(){
+    this.#cars.forEach((car, carIndex, carsArr) => {
+      const carPosition = {
+        top: car.element.offsetTop,
+        right: car.element.offsetLeft + car.element.offsetWidth,
+        bottom: car.element.offsetTop + car.element.offsetHeight,
+        left: car.element.offsetLeft,
+      };
+      if(carPosition.top > window.innerHeight){
+        car.remove();
+        carsArr.splice(carIndex, 1);
+        this.#carSpeed;
+        this.#intervalValue--;
+        console.log(this.#intervalValue);
+      }
+    })
   }
 }
 
