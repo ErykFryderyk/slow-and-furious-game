ad00c3cde114b39ddeedea71286dd50307f3f07d
Eryk Fryderyk <grzeniu098@gmail.com>
2021-04-25 15:01:12 +0200

    add method responsible form create and remove other cars

--- src/script/OtherCar.js
+++ src/script/OtherCar.js
@@ -1,24 +1,45 @@
 export class OtherCar {
-    constructor(container, otherCarClass){
+    constructor(container, intervalTime, carSpeed, otherCarClass){
         this.container = container;
+        this.intervalTime = intervalTime;
+        this.carSpeed = carSpeed;
         this.otherCarClass = otherCarClass;
         this.element = document.createElement('div');
     }
+    #carSpeed = 3;
     
     init(){
-        this.setCar();
+        this.#setCar();
+        this.#updatePosition();
     }
     
-    setCar(){
+    #setCar(){
+        this.element.classList.add('other-car');
         this.element.classList.add(this.otherCarClass);
-        this.element.style.top = '0px';
+        this.element.style.top = "-150px";
         this.element.style.left = `${this.#randomDrivingCarPosition()}px`;
         this.container.appendChild(this.element);
-
+    }
+    
+    #randomDrivingCarPosition(){
+        return Math.floor(
+            Math.random() * 
+            this.container.offsetWidth + 
+            this.container.getBoundingClientRect().left - 
+            (this.element.offsetWidth)
+        );
     }
 
-    #randomDrivingCarPosition(){
-        return Math.floor(Math.random() * window.innerWidth - this.element.offsetWidth);
+    #updatePosition(){
+        setInterval(() => this.#setNewPosition(), this.intervalTime);
+    }
+    #setNewPosition(){
+        this.element.style.top = `${this.element.offsetTop + this.carSpeed}px`
+    }
+
+    remove(){
+        clearInterval(this.interval);
+        this.element.remove();
     }
 }
 
