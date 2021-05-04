5fab5d925bfd7fa19eadf6a57876abb265bfacba
Eryk Fryderyk <grzeniu098@gmail.com>
2021-05-04 23:42:33 +0200

    create new inteval function and creating element about random car class

--- src/script/Game.js
+++ src/script/Game.js
@@ -17,6 +17,9 @@ class Game {
   #createOtherCarInterval = null;
   #carSpeed = 5;
   #intervalValue = 1500;
+  #carsClassArray = ['truck', 'pickup', 'van', 'taxi', 'audi', 'police'];
+
+
 
   init() {
     this.#car.init();
@@ -26,26 +29,30 @@ class Game {
   #newGame(){
     this.#otherCarsInterval = 10;
     // this.#createOtherCarInterval = setInterval(() => this.#randomNewOtherCar(), this.#intervalValue);
-    // this.#checkPositionInterval = setInterval(() => this.#checkPosition(), 1);
+    // this.$createOtherCarInterval = this.#loop();
+    this.#checkPositionInterval = setInterval(() => this.#checkPosition(), 1);
+  }
+
+  #loop(){
+    this.#intervalValue = Math.round(Math.random() * (1000-500)) + 500;
+      setTimeout(() => {
+            this.#randomNewOtherCar();
+            this.#loop();
+      }, this.#intervalValue);
   }
 
   #randomNewOtherCar(){
-    const randomNumber = Math.floor(Math.random() * 5) + 1;
+    let carClass;
+    let randomNumber = Math.floor(Math.random() * 5) + 1;
 
-    randomNumber % 5 
-    ? this.#createNewOtherCar(
-        this.#htmlElements.roadway,
+    carClass = this.#carsClassArray[randomNumber];
+
+    this.#createNewOtherCar(
+      this.#htmlElements.roadway,
         this.#otherCarsInterval,
-        this.#carSpeed, 
-        'pickup'
-      ) 
-    : 
-      this.#createNewOtherCar(
-        this.#htmlElements.roadway,
-        this.#otherCarsInterval, 
         this.#carSpeed,
-        'van'
-      );  
+        carClass, 
+    ); 
   }
 
   #createNewOtherCar(...params){
@@ -68,8 +75,8 @@ class Game {
       if(carPosition.top > window.innerHeight){
         car.remove();
         carsArr.splice(carIndex, 1);
-        this.#carSpeed += 0.2;
-        console.log(this.#carSpeed);
+        // this.#carSpeed += 0.2;
+        // console.log(this.#carSpeed);
       }
       if(
         this.#car.playerPosition.bottom >= carPosition.top && 
