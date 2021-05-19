1a8c63b8801ce7e194d1074496b4bcdedb6640ba
Eryk Fryderyk <grzeniu098@gmail.com>
2021-05-19 22:51:02 +0200

    add menu game

--- src/script/main.js
+++ src/script/main.js
@@ -1,16 +1,19 @@
+import { Game } from './Game.js';
+
+const startContainer = document.querySelector('[data-wrapper-game]');
+const selectCarsContainer = document.querySelector('[data-wrapper-select-cars]');
+const gameBoard = document.querySelector('[data-game-board]');
 const inputName = document.querySelector('[data-input-name]');
 const startGameBtn = document.querySelector('[data-start-game]');
+const playBtn = document.querySelector('[data-play-btn]');
 
-
-console.log(startGameBtn);
 
 const startGame = () => {
     if(inputName.value !== ''){
-        console.log('START!!!');
+        startContainer.classList.add('hide');
+        selectCarsContainer.classList.remove('hide');
     } else {
-        console.log('Wypełnij input');
         inputName.classList.add('input__game-start--error');
-
     }
 }
 
@@ -23,5 +26,14 @@ const checkError = () => {
 
 inputName.addEventListener('keyup', checkError);
 startGameBtn.addEventListener('click', startGame);
+playBtn.addEventListener('click', () => {
+    selectCarsContainer.classList.add('hide');
+    gameBoard.classList.remove('hide');
+
+    // window.onload = function () {
+        const game = new Game();
+        game.init();
+    // };
+});
 
 
