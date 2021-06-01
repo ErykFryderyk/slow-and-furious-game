2d0e31cf562c4ef0cd03c1c4a5f7cd3bdcb7a043
Eryk Fryderyk <grzeniu098@gmail.com>
2021-06-01 22:39:31 +0200

    modificates event in initGame method

--- src/script/main.js
+++ src/script/main.js
@@ -1,9 +1,8 @@
 import { Game } from './Game.js';
 
 class initializeGame {
-    constructor(){
+    gameObject = null; 
 
-    }
     startContainer = document.querySelector('[data-wrapper-game]');
     selectCarsContainer = document.querySelector('[data-wrapper-select-cars]');
     gameBoard = document.querySelector('[data-game-board]');
@@ -15,26 +14,38 @@ class initializeGame {
 
     initGame(){
         this.startGameBtn.addEventListener('click', () => {
+            this.selectCar()}
+        );
+
+        this.inputName.addEventListener('keyup', () => {
+            this.checkError()}
+        );
+
+        this.playBtn.addEventListener('click', () => {
             this.startGame();
         });
-        this.inputName.addEventListener('keyup', this.checkError());
-        this.startGameBtn.addEventListener('click', this.startGame());
-        this.playBtn.addEventListener('click', () => {
-            this.selectCarsContainer.classList.add('hide');
-            this.gameBoard.classList.remove('hide');
-
-            const game = new Game(gameBoard);
-            game.init();
+        this.playAgainBtn.addEventListener('click', () => {
+            console.log(this.gameObject);
+            this.gameObject = null;
+            this.selectCar();
         });
     }
-    startGame(){
-        console.log('Działą');
-        if(this.inputName.value !== ''){
+    selectCar(){
+        if(this.inputName.value != ''){
             this.startContainer.classList.add('hide');
             this.selectCarsContainer.classList.remove('hide');
+            this.modal.classList.add('hide');
         } else {
             this.inputName.classList.add('input__game-start--error');
         }
+    }
+    startGame() {
+        this.gameObject = new Game(this.gameBoard);
+
+        this.selectCarsContainer.classList.add('hide');
+        this.gameBoard.classList.remove('hide');
+
+        this.gameObject.init();
     }
     checkError(){
         if(this.inputName.value.length > 1){
@@ -42,23 +53,6 @@ class initializeGame {
         }
     }
 }
-
-
-// inputName.addEventListener('keyup', checkError);
-// startGameBtn.addEventListener('click', startGame);
-// playBtn.addEventListener('click', () => {
-//     selectCarsContainer.classList.add('hide');
-//     gameBoard.classList.remove('hide');
-
-//     const game = new Game(gameBoard);
-//     game.init();
-// });
-
-// playAgainBtn.addEventListener('click', () => {
-//     modal.classList.add('hide');
-//     selectCarsContainer.classList.remove('hide');
-// })
-
 
 window.onload = function(){
     const game = new initializeGame();
